302522c130aadd0f00665dd39764c21584a512a6
RinLovesYou <xxklintan123xx@gmail.com>
2022-11-22 08:29:23 +0100

    canonicalize path of def

--- proxy/build.rs
+++ proxy/build.rs
@@ -4,6 +4,7 @@ fn main() {
     #[cfg(target_os = "windows")]
     {
         let lib_path = Path::new("deps").join("Exports.def");
-        println!("cargo:rustc-cdylib-link-arg=/DEF:{}", lib_path.display());
+        let absolute_path = std::fs::canonicalize(&lib_path).unwrap();
+        println!("cargo:rustc-cdylib-link-arg=/DEF:{}", absolute_path.display());
     }
 }
